b7310a2d7d380edca5b1bef209e0b110dcf1a0dd
Infinite <afkid@hotmail.com>
2024-12-30 14:00:19 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -17,15 +17,14 @@ const protect_keylist = [
   "password",
 ]
 
-let index_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
-let result_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
+let index_html = "https://yugchak.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
+let result_html = "https://yugchak.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
 
 const html404 = `<!DOCTYPE html>
   <html>
   <body>
     <h1>404 Not Found.</h1>
     <p>The url you visit is not found.</p>
-    <p> <a href="https://github.com/crazypeace/Url-Shorten-Worker/" target="_self">Fork me on GitHub</a> </p>
   </body>
   </html>`
 
